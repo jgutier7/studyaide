6aed0f5a36f6331e42955b1f4893ecb5f1bda62c
jgutier7 <jgutier7@nd.edu>
2024-04-02 11:40:18 -0400

    Module: Updated View

--- src/Components/StudyModules/Module/Module.js
+++ src/Components/StudyModules/Module/Module.js
@@ -1,11 +1,15 @@
+// src/Components/Module/Module.js
 import React from 'react';
 import '../../../styles/Module.css';
 
 function Module({ module }) {
   const { title, daysUntilExam, studyStreak } = module;
   
+  // Determine the class based on the title
+  const moduleClass = title.replace(/\s+/g, '-').toLowerCase();
+  
   return (
-    <div className="module">
+    <div className={`module module-${moduleClass}`}>
       <h2>{title}</h2>
       <div className="info">
         <div className="days-until-exam">DAYS UNTIL EXAM: {daysUntilExam}</div>
