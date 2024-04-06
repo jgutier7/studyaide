25ce01d87254762dfac2e60f723aed904e2c39da
jgutier7 <jgutier7@nd.edu>
2024-04-06 14:39:26 -0400

    Whiteboard: Added Back Button

--- src/Components/Whiteboard/Whiteboard.js
+++ src/Components/Whiteboard/Whiteboard.js
@@ -1,12 +1,22 @@
 // src/Components/Whiteboard/Whiteboard.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Tldraw } from '@tldraw/tldraw';
-import '@tldraw/tldraw/tldraw.css'
+import '@tldraw/tldraw/tldraw.css';
 
 function Whiteboard() {
+  const navigate = useNavigate();
+
+  const handleBackToOverview = () => {
+    navigate('/'); 
+  };
+
   return (
     <div className="whiteboard" style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw isDarkMode="true" />
+      <button onClick={handleBackToOverview} style={{ position: 'absolute', top: 10, left: 10, zIndex: 1000 }}>
+        Back to Overview
+      </button>
+      <Tldraw isDarkMode={true} />
     </div>
   );
 }
