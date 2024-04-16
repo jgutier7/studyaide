cda5cf21540637f1653ad8339d26a3726e943c3b
Benjamin Brown <bbrown24@student10.cse.nd.edu>
2024-04-16 11:31:41 -0400

    google login stuff

--- src/App.js
+++ src/App.js
@@ -4,12 +4,53 @@ import Home from './Components/Home/Home';
 import Whiteboard from './Components/Whiteboard/Whiteboard';
 import Login from './Components/Login/Login';
 import './styles/App.css';
+import { useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import './styles/Login.css';
+
+
+
+
 
 function App() {
   const modules = [
     { title: 'AP Biology', daysUntilExam: 42, studyStreak: 21 },
     { title: 'AP US History', daysUntilExam: 46, studyStreak: 15 },
   ];
+
+  const [ user, setUser ] = useState([]);
+  const [ profile, setProfile ] = useState([]);
+
+  const login = useGoogleLogin({
+      onSuccess: (codeResponse) => setUser(codeResponse),
+      onError: (error) => console.log('Login Failed:', error)
+  });
+
+  useEffect(
+      () => {
+          if (user) {
+              axios
+                  .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                      headers: {
+                          Authorization: `Bearer ${user.access_token}`,
+                          Accept: 'application/json'
+                      }
+                  })
+                  .then((res) => {
+                      setProfile(res.data);
+                  })
+                  .catch((err) => console.log(err));
+          }
+      },
+      [ user ]
+  );
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+      googleLogout();
+      setProfile(null);
+  };
 
   return (
     <BrowserRouter>
@@ -19,6 +60,22 @@ function App() {
           <Route path="/whiteboard/:moduleClass" element={<Whiteboard />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+        {profile ? (
+                <div>
+                    <img src={profile.picture} alt="user image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+              <button class="google-signin-button" onClick={login}>
+                  <img src={require('./images/google-icon.png')} alt="Google sign-in" class="google-icon"/>
+                  Sign in with Google 
+              </button>
+            )}
       </div>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import App from "./App.js";
 
 const rootElement = document.getElementById("app");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="695583425567-mk5of6rtvo7vk8f6lpcecaj3is19p3m4.apps.googleusercontent.com">
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </GoogleOAuthProvider>,
   rootElement
 );
