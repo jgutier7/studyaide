d07dac1fb012da9bde70d1df870d03ea7e7d3665
Benjamin Brown <bbrown24@student10.cse.nd.edu>
2024-04-18 23:56:20 -0400

    login changes and homescreen layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Whiteboard from './Components/Whiteboard/Whiteboard';
 import Login from './Components/Login/Login';
+<<<<<<< Updated upstream
 import './styles/App.css';
 import { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+=======
+import { googleLogout } from '@react-oauth/google';
+>>>>>>> Stashed changes
 import axios from 'axios';
 import './styles/Login.css';
 
@@ -22,6 +26,7 @@ function App() {
   const [ user, setUser ] = useState([]);
   const [ profile, setProfile ] = useState([]);
 
+<<<<<<< Updated upstream
   const login = useGoogleLogin({
       onSuccess: (codeResponse) => setUser(codeResponse),
       onError: (error) => console.log('Login Failed:', error)
@@ -45,17 +50,49 @@ function App() {
       },
       [ user ]
   );
+=======
+  const onSuccess = (codeResponse) => {
+    setUser(codeResponse);
+    setRedirectToLogin(false); // Reset redirect on successful login
+  };
+
+  const onError = (error) => {
+    console.log('Login Failed:', error);
+  };
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+            headers: {
+                Authorization: `Bearer ${user.access_token}`,
+                Accept: 'application/json'
+            }
+        })
+        .then(res => setProfile(res.data))
+        .catch(err => console.log(err));
+    }
+  }, [user]);
+>>>>>>> Stashed changes
 
   // log out function to log the user out of google and set the profile array to null
   const logOut = () => {
+<<<<<<< Updated upstream
       googleLogout();
       setProfile(null);
+=======
+    googleLogout();
+    setUser(null);
+    setProfile(null);
+    setRedirectToLogin(true);  // Set flag to true to trigger redirection
+>>>>>>> Stashed changes
   };
 
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
+<<<<<<< Updated upstream
           <Route path="/" element={<Home modules={modules} />} />
           <Route path="/whiteboard/:moduleClass" element={<Whiteboard />} />
           <Route path="/login" element={<Login />} />
@@ -76,6 +113,19 @@ function App() {
                   Sign in with Google 
               </button>
             )}
+=======
+          <Route path="/" element={
+            profile ? (
+              <Navigate replace to="/home" />
+            ) : (
+              <Login onSuccess={onSuccess} onError={onError} />
+            )
+          } />
+          <Route path="/home" element={<Home modules={modules} profile={profile} logOut={logOut} />} />
+          <Route path="/whiteboard/:moduleClass" element={<Whiteboard />} />
+          {/*<Route path="/login" element={<Login login={onSuccess} />} />*/}
+        </Routes>
+>>>>>>> Stashed changes
       </div>
     </BrowserRouter>
   );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import StudyModules from '../StudyModules/StudyModules';
+import '../../styles/Home.css';
 
-const Home = ({ modules }) => {
+
+const Home = ({ modules, profile, logOut }) => {
   return (
     <div className="home">
       <div className="navigation">
         <button className="overview">Overview</button>
         <button className="calendar">Calendar</button>
       </div>
+      {profile && (
+        <div>
+          <img src={profile.picture} alt="user" id="profile_pic"/>
+          <p id="profile_name">{profile.name}</p>
+          <button onClick={logOut} id="logout">Log out</button>
+        </div>
+      )}
       <StudyModules modules={modules} />
       <button className="add-module">+ Add Module</button>
     </div>
