7dd5a114e163281de440e15454910a2e372ce7d7
lschantz <lschantz@nd.edu>
2024-04-23 21:19:01 -0400

    study preferences added

---
+++ src/Components/StudyPreferences/StudyPreferences.js
@@ -0,0 +1,12 @@
+//StudyPreferences.js
+const StudyPreferences = {
+  preferredDays: ['Monday', 'Wednesday', 'Friday'],
+  preferredTimes: ['Morning', 'Afternoon'],
+  sessionLength: '60',
+  modulePreferences: [
+    { title: 'AP Biology',  daysUntilExam: 42, intensity: 3 },
+    { title: 'AP US History', daysUntilExam: 46, intensity: 2 },
+  ],
+};
+
+export default StudyPreferences;
